19e8009e8a95e25eb50f3d93b4182e99e29f9a36
faraznazeer <farznaz98@gmail.com>
2018-11-16 23:37:15 +0530

    Commited with changes

--- model.py
+++ model.py
@@ -22,36 +22,35 @@ def get_data(filename):
                 temp.append(  float ( data[11] )  )
                 d = d+1
             c = (c+1)%50
-    print(d)
 
 
 
-def predict_temp(dates, price, x):
+def predict_temp(x):
 
     
-    #lin = SVR( kernel="linear", C=1)
-    #poly = SVR( kernel="poly", C=1, degree=2)
-    rbf = SVR( kernel="rbf", C=1000, gamma=0.1)
+    lin = SVR( kernel="linear", C=1)
+    poly = SVR( kernel="poly", C=1, degree=2)
+    rbf = SVR( kernel="rbf", C=100, gamma=0.05)
 
-   # lin.fit(index,temp)    
-    #poly.fit(index,temp)
+    lin.fit(index,temp)    
+    poly.fit(index,temp)
     rbf.fit(index,temp)
 
     plt.scatter(index, temp, color = "black", label = "Data")
-    #plt.plot( index, lin.predict(index), color = "red", label = "Linear")
-   # plt.plot( index, poly.predict(temp), color = "green", label = "Polynomial")
+    plt.plot( index, lin.predict(index), color = "red", label = "Linear")
+    plt.plot( index, poly.predict(index), color = "green", label = "Polynomial")
     plt.plot( index, rbf.predict(index), color = "blue", label = "RBF")
 
-    plt.xlabel("Dates")
+    plt.xlabel("Dates (Data Point)")
     plt.ylabel("Temperature")
     plt.legend()
     plt.show()
 
-    #return  lin.predict(x)[0], poly.predict(x)[0], rbf.predict(x)[0]
+    return  lin.predict(x)[0], poly.predict(x)[0], rbf.predict(x)[0]
 
 
 get_data("Delhi_Weather_Data.csv")
 
-print( predict_temp(dates,temp,100317) )
+print( "Temperature at data point 2002 : "+str(predict_temp(2002)) )
 
 #print ( get_accuracy() )

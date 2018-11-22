026f0f97b977b942637aa616499013c6b846a1b2
faraznazeer <farznaz98@gmail.com>
2018-11-23 02:14:19 +0530

    RBF only

--- model.py
+++ model.py
@@ -1,56 +1,86 @@
 import csv
 from sklearn.svm import SVR
 import numpy as np
-import pandas as pd
 import matplotlib.pyplot as plt 
+import time
 
-index = list(range(2002))
-index = np.reshape( index , (2002, 1) )
-dates = []
+print("\n Packages Loaded  ")
+
 temp = []
+dates = []
 
 def get_data(filename):
     
+    n = 0
+    c = 1
+    avg = 0
     with open(filename, "r") as file:
         Reader = csv.reader(file)
         next(Reader)
-        c = 0
-        d=0
         for data in Reader:
-            if data[11] != "" and c == 0:
-                dates.append(  data[0]  )
-                temp.append(  float ( data[11] )  )
-                d = d+1
-            c = (c+1)%50
+            if data[11] != "":
+                if(c > 0 ):
+                    avg = avg + float(data[11])
+                    current_date = data[0]
+                else:
+                    dates.append(  current_date  )
+                    temp.append(  float ( avg/24 )  )
+                    n = n + 1
+                    avg = 0
+                c = (c + 1)%24
+
+        dates.append(  current_date  )
+        temp.append(  float ( avg/c )  )
+        n = n + 1
+
+    index = np.reshape( range(n), (n,1) )
+
+    return n, index 
 
 
+def TrainModel(index, temp):
 
-def predict_temp(x):
-
-    
-    lin = SVR( kernel="linear", C=1)
-    poly = SVR( kernel="poly", C=0.00001, degree=2)
-    rbf = SVR( kernel="rbf", C=100, gamma=0.05)
-
-    lin.fit(index,temp)    
-    poly.fit(index,temp)
+    rbf = SVR( kernel="rbf", C=1e3, gamma = 0.01)
     rbf.fit(index,temp)
+    return rbf
 
-    plt.scatter(index, temp, color = "black", label = "Data")
-    plt.plot( index, lin.predict(index), color = "red", label = "Linear")
-    plt.plot( index, poly.predict(index), color = "green", label = "Polynomial")
-    plt.plot( index, rbf.predict(index), color = "blue", label = "RBF")
 
+def PlotTestDataPrediction(model, index, temp):
+    #print(test_temp)
+    plt.scatter(index , temp, color = "black", label = "Data")
+    plt.plot( index , model.predict(index), color = "blue", label = "RBF")
     plt.xlabel("Dates (Data Point)")
     plt.ylabel("Temperature")
     plt.legend()
     plt.show()
-        
-    return  lin.predict(x)[0], rbf.predict(x)[0], poly.predict(x)[0]
 
 
-get_data("Delhi_Weather_Data.csv")
 
-print( "Temperature at data point 2002 : "+str(predict_temp(2003)) )
+def predict(model, datapoint):
+    return model.predict(datapoint)[0]
 
-#print ( get_accuracy() )
+
+
+print("\n Loading Data.........")
+n, index = get_data("Delhi_Weather_Data.csv")
+print("\n Data Successfully Loaded !!!")
+
+
+print("\n Training Begins...........")
+start = time.time()
+
+model = TrainModel(index, temp)
+
+duration = time.time() - start 
+print("\n Training Completed in " + str(duration)+ " Seconds" )
+
+
+PlotTestDataPrediction(model, index, temp)
+
+print( "\n Temperature at next data point " + str(n+1) +" : "+str(predict( model, n + 1)) )
+
+datapoint = input("\n Enter the data point to predict : " )
+
+print( "\n Temperature at data point " +str(datapoint)+ ": " + str(predict( model, datapoint)) )
+
+
